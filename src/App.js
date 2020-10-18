eec5300a69da0bc97dbed75a12f524f997e4c658
Aniket Raj <aniketronaldo10@gmail.com>
2020-10-18 19:58:49 +0530

    Created structure and finalized looks of page
    Quite a lot of 'sass' (pun intended xD)
    In terms of React, it is pretty much what I have already learnt, except now I know it is always better to destructure the object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './components/homepage.component.jsx'
+import Homepage from './pages/homepage/homepage.component.jsx'
 
 function App() {
   return (
